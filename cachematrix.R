7d684cf753866ebaebb5910fa2aa9220a4a7c5ba
Luis Diego Ruiz <oracleruiz@gmail.com>
2015-01-22 23:15:04 -0600

    First version completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,44 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixCached <- NULL
+  matrixInvertCached <- NULL
+  set <- function(y) {    
+     if(is.null(matrixCached) || !identical(matrixCached,y)){
+       message("New Matrix value set")
+       x <<- y
+       matrixCached <<- y
+       matrixInvertCache <<- NULL      
+    }
+    else
+    {
+      message("Matrix has not changed.")
+    }
+  }
+  get <- function() matrixCached
+  
+  setInverseMatrix <- function(matrix) matrixInvertCached <<- matrix
+  
+  getInverseMatrix <- function() matrixInvertCached
+  
+  list(set = set, get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixInvert <- x$getInverseMatrix()
+  
+  if(!is.null(matrixInvert)) {
+    message("getting cached inverted matrix")
+    return(matrixInvert)
+  }  
+  message("calculating inverted matrix")
+  invert <- solve(x$get())
+  x$setInverseMatrix(invert)
+  invert  
 }
+
